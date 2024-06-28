95951f5a06b387926586ae3c0c6f6c3170872c4a
saidjon <saidjons@bk.ru>
2024-06-29 01:11:06 +0500

    longman logger extension

--- src/browser_action/popp.js
+++ src/browser_action/popp.js
@@ -1,15 +1,28 @@
+
+// popup html js file 
+
+
 function responseCallback(res) {
   console.log("response:${res}");
 }
+
+
+
 console.log("POP UP HELLO");
+
+
 document.addEventListener("DOMContentLoaded", function (dcle) {
   id = "emjafccejmcocnnanicjfipccihahhfn";
-
+  // run functions here
+   
+  syncCounter()
+  logData()
+  clearData()
   btn = document.getElementById("btn");
-  note = document.getElementById("note");
-  console.log(note.value);
+  // note = document.getElementById("note");
+  // console.log(note.value);
   let tabId = null;
- ;
+   
 
   // Open up connection
 
@@ -20,28 +33,128 @@ document.addEventListener("DOMContentLoaded", function (dcle) {
       tabId = t[0].id;
     });
 
-    console.log("clicked", note.value);
-    setTimeout(async()=>console.log(
-      await window.navigator.clipboard.readText()), 3000)
-   
-
-      let tab = null;
-
-      chrome.tabs.query(
-        { active: true, lastFocusedWindow: true },
-        function (t) {
-          tab = t[0];
-        }
-      );
+    // setTimeout(async()=>console.log(
+    //   await window.navigator.clipboard.readText()), 3000)
 
 
-      setTimeout(() => {
-        
-      chrome.tabs.sendMessage(tab.id, { greeting: note.value }, function (res) {
+    let tab = null;
+
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (t) {
+        tab = t[0];
+      }
+    );
+
+    // recieve scraped word
+    setTimeout(() => {
+
+      chrome.tabs.sendMessage(tab.id, { action: "scrape" }, function (res) {
         console.log(res);
+        //  setStorage("longman",JSON.stringify(res.extraction))
+        //  get chrome storage
+
+
+        setCounter(res.counter)
       });
     }, 50);
 
-   
+
   });
 });
+
+ 
+
+const clearData = function () {
+  let updateBTN = document.querySelector('#clearBTN')
+  updateBTN.addEventListener("click", function () {
+    let tab = null;
+
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (t) {
+        tab = t[0];
+      }
+    );
+    // recieve message
+    setTimeout(() => {
+
+      chrome.tabs.sendMessage(tab.id, { action: "clearData" }, function (res) {
+        console.log(res);
+        //  setStorage("longman",JSON.stringify(res.extraction))
+        //  get chrome storage
+
+
+        setCounter(res.counter)
+      });
+    }, 50);
+
+
+  });
+}
+const logData = function () {
+  let updateBTN = document.querySelector('#logBTN')
+  updateBTN.addEventListener("click", function () {
+    let tab = null;
+
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (t) {
+        tab = t[0];
+      }
+    );
+    // recieve message
+    setTimeout(() => {
+
+      chrome.tabs.sendMessage(tab.id, { action: "logData" }, function (res) {
+        console.log(res);
+        //  setStorage("longman",JSON.stringify(res.extraction))
+        //  get chrome storage
+
+
+        setCounter(res.counter)
+      });
+    }, 50);
+
+
+  });
+}
+
+
+const syncCounter = function () {
+  let updateBTN = document.querySelector('#updateBTN')
+  updateBTN.addEventListener("click", function () {
+    let tab = null;
+
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (t) {
+        tab = t[0];
+      }
+    );
+    // recieve scraped word
+    setTimeout(() => {
+
+      chrome.tabs.sendMessage(tab.id, { action: "sync" }, function (res) {
+        console.log(res);
+        //  setStorage("longman",JSON.stringify(res.extraction))
+        //  get chrome storage
+
+
+        setCounter(res.counter)
+      });
+    }, 50);
+
+
+  });
+}
+
+
+
+
+
+const setCounter = function (counter) {
+  let dom = document.querySelector('span')
+  dom.textContent = counter
+
+}
--- src/content/content.js
+++ src/content/content.js
@@ -1,42 +1,147 @@
-// content.js
-// alert('Hello, world!');
+//document object
 
-console.log('conent js');
+
+// localStorage.setItem("longman-counter", JSON.stringify([]));
+// localStorage.setItem("longman-words", JSON.stringify([]));
 
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
+        console.info("request", request);
 
-  
+        switch (request.action) {
+            case "scrape": scrape(); sendResponse({ counter: getCounter() });
 
-        console.log(request,'saidjon');
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-        sendResponse({farewell: "goodbye"});
+                break;
+
+            case "logData": logData();sendResponse({ message: "logged" });
+
+            break;
+            case "sync": sendResponse({ counter: getCounter() });
+
+                break;
+            case "clearData": clearData();sendResponse({ counter: getCounter() });
+
+                break;
+
+            default:
+                break;
+        }
+
+
+        //   console.log(sender.tab ?
+        //               "from a content script:" + sender.tab.url :
+        //               "from the extension");
+
     });
 
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.color === "green") {
-//       document.body.style.backgroundColor = "green";
-//       sendResponse({ status: "done" });
-//     }
-//   });
+const clearData = function () {
+    let words = localStorage.setItem("longman-words",JSON.stringify([]));
+    console.info("longman-words cleared")
+}
+const logData = function () {
+    let words = localStorage.getItem("longman-words");
+    let parsed = JSON.parse(words)
+    console.info(`count : ${parsed.length}`);
+    console.info(words)
+}
+const pushWord = function (word) {
+    let wordsJSON = localStorage.getItem("longman-words");
+    let words = JSON.parse(wordsJSON)
+    
+     let check = isWordExists(word,words)
+     if(check ==false){
+         words.push(word)
+         localStorage.setItem("longman-words", JSON.stringify(words));
+        return;
+     }
+
+     alert("word exists "+ word.word)
+ 
+
+     
+}
+const isWordExists = function (obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i].word === obj.word) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const getCounter = function () {
+    let words = localStorage.getItem("longman-words");
+    return JSON.parse(words).length
+}
+const scrape = function () {
+    let extraction = extractWord()
+    console.info(extraction);
+    pushWord(extraction)
+}
+
+function extractWord() {
+
+    let word = document.querySelector("h1.pagetitle").textContent;
+    let translations = []
+
+    let pronounciations = []
+    let pronounsDom   = document.querySelector('.dictionary > .dictentry').querySelectorAll('.Head .speaker')
+
+    // let pronounsDom   = document.querySelectorAll(".Head .speaker")
+    if(pronounsDom.length){
+        pronounsDom.forEach(el => {
+            let link  = el.getAttribute("data-src-mp3")
+            if(link){
+                pronounciations.push(link)
+            }
+        });
+    }
+
+    let pos = ''
+    if (document.querySelector('span.POS')) {
+        pos = document.querySelector('span.POS').textContent ?? ""
+    };
+    if (!pos && document.querySelector('span.GRAM')) {
+        pos = document.querySelector('span.GRAM').textContent ?? ""
+    };
+    pos.trim()
+
+    let translationsDom = document.querySelectorAll('.dictlink > span .Sense')
+
+    if (translationsDom.length) {
+
+        translationsDom.forEach(dom => {
+            let tr = ""
+            if (!dom.querySelector('span .DEF')) {
+                return;
+            }
+            tr = dom.querySelector('span .DEF').textContent
+            let audioDoms = dom.querySelectorAll('span .EXAMPLE')
+
+            let ex = []
+            //  getting audios and examples 
+            for (var i = 0; i < audioDoms.length; i++) {
+                audioDoms[i]
+                let text = audioDoms[i].textContent.trim() ?? ""
+                let audio = ""
+                if (audioDoms[0].querySelector('span') && audioDoms[0].querySelector('span').getAttribute("data-src-mp3")) {
+
+                    audio = audioDoms[0].querySelector('span').getAttribute("data-src-mp3")
+                }
+                ex.push({ text, audio })
+            };
+
+            translations.push({ tr, ex });
+
+        });
+
+    };
+    return { pos,word,prouncitions:pronounciations, defs:translations }
+
+}
 
 
-//   chrome.runtime.onConnect.addListener(function (port) {
-//     port.onMessage.addListener(function (msg) {
-//       if (port.name === "uiOps") {
-//         const idToQuery = msg.id;
-//         if (document.getElementById(idToQuery)) {
-//           port.postMessage({
-//             exists: true,
-//           });
-//         } else {
-//           port.postMessage({
-//             exists: false,
-//           });
-//         }
-//       }
-//     })
-//     })
+
